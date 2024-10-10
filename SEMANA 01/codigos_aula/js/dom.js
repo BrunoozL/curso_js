09fd95e2885bb6d200bb768d87dc24f98883c5d4
Bruno Righi <brunorighi267@gmail.com>
2024-10-10 09:22:19 -0300

    Terceiro commit

--- SEMANA 01/codigos_aula/js/dom.js
+++ SEMANA 01/codigos_aula/js/dom.js
@@ -1,3 +1,4 @@
+// Função CRIADA, mas não está sendo UTILIZADA!
 function verElemento()
 {
     var elemento = document.getElementById("titulo");
@@ -5,3 +6,42 @@ function verElemento()
     console.log("TAG: ", elemento.nodeName);
     console.log("TEXTO: ", elemento.textContent);
 }
+
+function mudarCorTexto()
+{
+    // Pega o ID que usuário atribuiu no input
+    var elementoDesejado = document.getElementById("elemento").value;
+
+    // Busca o elemento que o usuário deseja alterar
+    var elemento = document.getElementById("elementoDesejado");
+    elemento.style.color = "#ff0000";
+}
+
+function mudarCorFundo()
+{
+    var elementoDesejado = document.getElementById("elemento").value;
+    
+    var elemento = document.getElementById("elementoDesejado");
+    elemento.style.backgroundColor = "yellow";
+}
+
+function adicionarComWrite()
+{
+    var textoDoImput = document.getElementById("elemento").value;
+
+    var texto = document.getElementById("texto");
+
+    document.write(textoDoImput);
+}
+
+function adicionarComInner()
+{
+    var textoDoImput = document.getElementById("elemento").value;
+
+    var texto = document.getElementById("texto");
+
+    texto.innerHTML += textoDoImput;
+}
+
+// Escrevendo apenas o seu nome, executa ela
+verElemento();
